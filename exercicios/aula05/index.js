a1844df6560f4a0fa8283a1c6f46d63cbfc86a2d
erick69aaaa <esb12@aluno.ifnmg.edu.br>
2026-04-09 17:18:04 +0000

    feat: pratica fundamentos de JavaScript no Node - aula 05

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -6,26 +6,59 @@
 // Não mexa no arquivo index.test.js.
 
 // Exercício 1 — classificarIdade
-export function classificarIdade(idade) {
-  // escreva seu código aqui
+export function classificarIdade(idade) { 
+ if (idade < 12) {
+    return "criança";
+  } else if (idade <= 17) {
+    return "adolescente";
+  } else {
+    return "adulto";
+  }
 }
 
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
-  // escreva seu código aqui
+  const desconto = (preco * percentual) / 100;
+  return preco - desconto;
 }
 
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
-  // escreva seu código aqui
+  if (nota >= 60) {
+    return "aprovado";
+  } else if (nota >= 40) {
+    // Como o primeiro 'if' já pegou quem tem 60 ou mais,
+    // aqui só entram as notas entre 40 e 59.
+    return "recuperação";
+  } else {
+    return "reprovado";
+  }
 }
+
 
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
-  // escreva seu código aqui
+  let soma = 0;
+  
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+      soma += numeros[i];
+    }
+  }
+  
+  return soma;
 }
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
-  // escreva seu código aqui
+  let contador = 0;
+
+  // O for...of percorre cada caractere da string diretamente
+  for (const caractere of texto) {
+    if (caractere === letra) {
+      contador++;
+    }
+  }
+
+  return contador;
 }
